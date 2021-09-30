c7339ba07ab6404e91820c38f43b22c527880697
Medimax <hadi.sakali@gmail.com>
2021-09-30 20:31:18 +0200

    Implement SupplyChain.sol

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -9,19 +9,28 @@ contract TestSupplyChain {
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
 
-    // buyItem
+    // For reference, I used this link : https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-solidity 
 
-    // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+    // buyItem
+    function testBuyItem() public {
+        SupplyChain sc = SupplyChain(DeployedAddresses.SupplyChain());
+        // test for failure if user does not send enough funds
+        // test for purchasing an item that is not for Sale
+        bool r;
+        (r, ) = address(sc).call(abi.encodeWithSignature("buyItem(uint)",100));
+        Assert.isFalse(r, "If this is true, something is broken!");
+    }  
 
     // shipItem
-
-    // test for calls that are made by not the seller
-    // test for trying to ship an item that is not marked Sold
-
+    function shipItem() public {
+        // test for calls that are made by not the seller
+        // test for trying to ship an item that is not marked Sold
+    }
+    
     // receiveItem
-
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
-
+    function receiveItem() public {
+        // test calling the function from an address that is not the buyer
+        // test calling the function on an item not marked Shipped
+    }
+    
 }
